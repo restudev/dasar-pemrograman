f0d1478ad35f4cc670367848d5f16e53ab50a196
restulm <122165425+restulm@users.noreply.github.com>
2023-01-18 09:11:33 +0700

    minggu1

---
+++ minggu1/Latihan C++/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "                     BIODATA DIRI" << endl;
+    cout << "============================================================" << endl;
+    cout << "Nama       : Restu Lestari Mulianingrum" << endl;
+    cout << "NIM        : A11.2022.14668" << endl;
+    cout << "Alamat     : Ds. Randusari, Kec. Pagerbarang, Kab. Tegal" << endl;
+    cout << "TTL        : Tegal, 3 Juni 2004" << endl;
+    cout << "Hobi       : Menggambar" << endl;
+    cout << "============================================================" << endl;
+    return 0;
+}
---
+++ minggu1/Latihan C/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf ("                     BIODATA DIRI\n");
+   printf ("=======================================================\n");
+   printf ("Nama    : Restu Lestari Mulianingrum\n");
+   printf ("NIM     : A11.2022.14668\n");
+   printf ("Alamat  : Ds. Randusari, Kec. Pagerbarang, Kab. Tegal\n");
+   printf ("TTL     : Tegal, 3 Juni 2004\n");
+   printf ("Hobi    : Menggambar\n");
+   printf ("=======================================================\n");
+   return 0;
+}
